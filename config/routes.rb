61bab85650ad70cc38f3eb1daa1edd18f025ebb8
Gardell Robinson <GardellR@Gmail.com>
2015-01-15 20:10:00 -0500

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
+  Rails.application.routes.draw do 
+    root to: 'visitors#new' 
+  end
+
+  #root to: 'vistors#new'
+  #root to: redirect('/about.html')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
